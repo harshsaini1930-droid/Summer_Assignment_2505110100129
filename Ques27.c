ebe4353ac1eead307f66623db5cb8da7bb3d425e
Harsh saini <harshsaini1930@gmail.com>
2026-06-07 22:03:11 +0530

    Day 7 uplodaded.

--- Ques27.c
+++ Ques27.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
-int main()
-{
-    int num,sum;
-    printf(" enter the num =");
-    scanf("%d",&num);
-    int sumDigits(int n)
+ int sumDigits(int n)
     {
         if(n==0)
         return 0;
         return (n%10)+ sumDigits(n/10);
     }
+int main()
+{
+    int num,sum;
+    printf(" enter the num =");
+    scanf("%d",&num);
+   
     printf(" sum of digits =%d",sumDigits(num));
     return 0;
 }
--- Ques28.c
+++ Ques28.c
@@ -1,10 +1,5 @@
 #include<stdio.h>
-int main()
-{
-    int num;
-    printf(" enter the num =");
-    scanf("%d",&num);
-    int rev = 0;
+ int rev = 0;
     int reverse(int n)
     {
         if(n==0)
@@ -12,6 +7,12 @@ int main()
         rev = rev*10 + n%10;
         return reverse(n/10);
     }
+int main()
+{
+    int num;
+    printf(" enter the num =");
+    scanf("%d",&num);
+   
     printf(" reverse =%d\n",reverse(num));
     return 0;
 }
