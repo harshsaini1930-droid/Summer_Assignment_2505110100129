c5e2367fd877c8b43431b87a36a1c00335e68e6b
Harsh saini <harshsaini1930@gmail.com>
2026-06-09 21:44:11 +0530

    initial commit

---
+++ __ques35revcharpattern.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,num;
+    printf(" enter the num =");
+    scanf("%d",&num);
+    for(i=1;i<=num;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            
+            printf("%c",'A' + i -1);
+        }
+        printf("\n");
+    }
+    return 0;
+}
